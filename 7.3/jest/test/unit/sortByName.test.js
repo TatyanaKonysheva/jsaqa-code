e72ceb5909f5379d0a07d69057110840931d176e
Konysheva Tatyana <tkonyseva693@gmail.com>
2023-10-17 17:57:30 +0300

    Added code

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,16 +2,34 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+
+    const result = sorting.sortByName(input);
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+    expect(result).toEqual(expected);
+  });
+
+  it("Books names should be not sorted", () => {
+    const input = [
+      "Я могу читать сам",
+      "Я могу читать сам",
+      "Я могу читать сам",
+    ];
+
+    const result = sorting.sortByName(input);
+    const expected = [
+      "Я могу читать сам",
+      "Я могу читать сам",
+      "Я могу читать сам",
+    ];
+    expect(result).toEqual(expected);
   });
 });
